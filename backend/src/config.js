55ec119d855ba2d553d1eef497e554981b6ed37e
aymen <jakebi@protonmail.com>
2022-12-27 23:38:09 +0100

    j

--- backend/src/config.js
+++ backend/src/config.js
@@ -84,7 +84,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xCD650C4b222ce3441d9931EEE7840aeC79b31f1D"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
