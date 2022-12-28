87b3e184fb403d4bcd34251c8088be5e08446790
aymen <jakebi@protonmail.com>
2022-12-28 14:08:50 +0100

    h

--- backend/src/config.js
+++ backend/src/config.js
@@ -73,13 +73,13 @@ const MINT_PRICE = 98; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, P
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-12-27T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-12-25T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-12-27T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-12-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xE191832F3C3DeC0519230e065724147d1643104F"; // Address that will receive the royalty
-const BASE_URI = null; // only update if you want to manually set the base uri
+const BASE_URI = "ipfs://bafybeiean3bdmdohwkpledbfxjf3pbowlpjiqcwzfkocv7l6h6pjfjnpq4"; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = ""; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
